7705c8c9cfadf994d7effa022009de5a8e6a0d99
gagachang <vivahavey@gmail.com>
2019-10-26 22:58:03 +0800

    Modify image path

--- src/rtsp.py
+++ src/rtsp.py
@@ -3,8 +3,8 @@ import json
 import time
 
 # upload picture to sensor
-device_id = '19151543448'
-api_url = 'https://iot.cht.com.tw/iot/v1/device/{0}/snapshot'.format(device_id)
+deviceID = '19151543448'
+api_url = 'https://iot.cht.com.tw/iot/v1/device/{0}/snapshot'.format(deviceID)
 
 headers = {
     'CK': 'PKYMWHKYFR1ZK2K0YE'}
@@ -12,7 +12,7 @@ headers = {
 files = {
     'meta': (None, json.dumps({"id": "camera1", "value": ["carTest"]}),
              'application/json'),
-    'file': ('test-image', open('./img/car.jpg', 'rb'), 'image/jpeg')}
+    'file': ('test-image', open('../static/img/car2.jpg', 'rb'), 'image/jpeg')}
 
 response = requests.post(api_url, files=files, headers=headers)
 print(response.text.encode('ascii'))
@@ -28,7 +28,7 @@ headers = {
 }
 
 response = requests.get(apiURL, headers=headers)
-with open("./testpic.jpg", 'wb') as f:
+with open("../static/img/car1.jpg", 'wb') as f:
     f.write(response.content)
 
 # upload picture to car detection api, then evaluate/upload parking lots information
@@ -38,7 +38,7 @@ headers = {
     "X-API-KEY": "ea522b8e-696e-4084-b9eb-97fdbd677d52",
 }
 
-files = {"file": open("./testpic.jpg", "rb")}
+files = {"file": open("../static/img/car2.jpg", "rb")}
 
 response = requests.post(apiURL, files=files, headers=headers)
 print(response.text)  # Here we get how many cars parking in the camera area
